144fdd79c7cadeba7c9b0a15fa2c749c092a296d
Sean Brewer <seabre986@gmail.com>
2014-01-12 16:42:47 -0500

    beat -> grid

--- src/osc_multitoggle/core.clj
+++ src/osc_multitoggle/core.clj
@@ -2,7 +2,7 @@
   (:use [overtone.osc :only [osc-handle]]
         [clojure.string :only (split)]))
 
-(def beat (atom {}))
+(def grid (atom {}))
 
 (defn- path-params [path]
   (rest (split path #"/")))
@@ -22,18 +22,18 @@
 (defn- change-column-map [m layout row column value]
   (assoc (get m layout) column (assoc (get (get m layout) column) row value)))
 
-(defn- update-beat [msg]
+(defn- update-grid [msg]
   (let [layout (get-layout-params (get msg :path))
         row (get-row-params (get msg :path))
         column (get-column-params (get msg :path))
         value (first (:args msg))]
-    (swap! beat assoc layout (change-column-map @beat layout row column value))))
+    (swap! grid assoc layout (change-column-map @grid layout row column value))))
 
 (defn get-active-in-row [layout column]
-  (let [rows (get (get @beat layout) column)]
+  (let [rows (get (get @grid layout) column)]
     (keys (select-keys rows (for [[k v] rows :when (= v 1.0)] k)))))
 
 (defn multitoggle-handler [server layout widget rows columns]
   (doseq [column (range 1 (+ columns 1))]
     (doseq [row (range 1 (+ rows 1))]
-      (osc-handle server (path-from-params layout widget row column) update-beat))))
+      (osc-handle server (path-from-params layout widget row column) update-grid))))
