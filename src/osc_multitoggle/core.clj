675ce33d9f922bdfe8aad6146c03ceba5dabb41a
Sean Brewer <seabre986@gmail.com>
2014-01-12 18:45:57 -0500

    Change grid atom map nesting to layout -> widget -> column -> row. Add remove-multitoggle-handler to allow removal of handlers and to clear grid atom of widget data from associated handler.

--- src/osc_multitoggle/core.clj
+++ src/osc_multitoggle/core.clj
@@ -1,5 +1,5 @@
 (ns osc-multitoggle.core
-  (:use [overtone.osc :only [osc-handle]]
+  (:use [overtone.osc :only [osc-handle osc-rm-handler]]
         [clojure.string :only (split)]))
 
 (def grid (atom {}))
@@ -10,6 +10,9 @@
 (defn- get-layout-params [path]
   (first (path-params path)))
 
+(defn- get-widget-params [path]
+  (second (path-params path)))
+
 (defn- get-row-params [path]
   (last (butlast (path-params path))))
 
@@ -19,21 +22,28 @@
 (defn- path-from-params [layout widget row column]
   (str "/" layout "/" widget "/" row "/" column))
 
-(defn- change-column-map [m layout row column value]
-  (assoc (get m layout) column (assoc (get (get m layout) column) row value)))
+(defn- change-column-map [m layout widget row column value]
+  (assoc-in (get m layout) [widget column row] value))
 
 (defn- update-grid [msg]
   (let [layout (get-layout-params (get msg :path))
         row (get-row-params (get msg :path))
         column (get-column-params (get msg :path))
+        widget (get-widget-params (get msg :path))
         value (first (:args msg))]
-    (swap! grid assoc layout (change-column-map @grid layout row column value))))
+    (swap! grid assoc layout (change-column-map @grid layout widget row column value))))
 
-(defn get-active-in-row [layout column]
-  (let [rows (get (get @grid layout) column)]
+(defn get-active-in-row [layout widget column]
+  (let [rows (get-in @grid [layout widget column])]
     (keys (select-keys rows (for [[k v] rows :when (= v 1.0)] k)))))
 
 (defn add-multitoggle-handler [server layout widget rows columns]
   (doseq [column (range 1 (+ columns 1))]
     (doseq [row (range 1 (+ rows 1))]
       (osc-handle server (path-from-params layout widget row column) update-grid))))
+
+(defn remove-multitoggle-handler [server layout widget rows columns]
+  (doseq [column (range 1 (+ columns 1))]
+    (doseq [row (range 1 (+ rows 1))]
+      (osc-rm-handler server (path-from-params layout widget row column))))
+  (swap! grid assoc (str layout) (dissoc (get @grid (str layout)) widget)))
