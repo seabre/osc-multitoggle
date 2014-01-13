870af83bbe60cc0d2cd49412bfdfc70f29833ee0
Sean Brewer <seabre986@gmail.com>
2014-01-12 19:30:17 -0500

    Modify project.clj with better description and version number.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject osc-multitoggle "0.1.0-SNAPSHOT"
-  :description "Easily keep track of TouchOSC multitoggle state"
-  :url "http://example.com/FIXME"
+(defproject osc-multitoggle "0.1.0"
+  :description "Easily keep track of TouchOSC multitoggle state, meant for use in Overtone."
+  :url "https://github.com/seabre/osc-multitoggle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
